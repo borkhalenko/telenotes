94cfd105aae1ca852e794d8667b3c576cb0b9de6
Borkhalenko Oleg <borkhalenko@gmail.com>
2019-10-31 01:49:38 +0200

    just save...

--- telenotes/telenotes_bot.py
+++ telenotes/telenotes_bot.py
@@ -1,10 +1,12 @@
 import telebot
+import telegram
+import telegram.ext
 import telenotes.settings as ts
 
 
 class TelenotesBot:
     #_bot = telebot.TeleBot(ts.bot_settings['TOKEN'])
-
+    '''
     def __init__(self, settings):
         self._bot = telebot.TeleBot(settings['TOKEN'])
 
@@ -14,3 +16,6 @@ class TelenotesBot:
 #    @_bot.message_handler(content_types=['text'])
     def repeat_all_messages(self, message):
         self._bot.send_message(message.chat.id, message.text)
+    '''
+    updater = telegram.ext.Updater(ts.bot_settings['TOKEN'], use_context=True)
+    print(bot.get_me())
