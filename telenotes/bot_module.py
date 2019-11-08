4d0b7f8fd439e7691b9cb3a9638980b9b7818d00
Borkhalenko Oleg <borkhalenko@gmail.com>
2019-11-08 02:09:07 +0200

    added echo func

--- telenotes/bot_module.py
+++ telenotes/bot_module.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 import telenotes.settings as settings
 
 
@@ -8,9 +8,13 @@ def hello(update, context):
 def start(update, context):
     update.message.reply_text("Started...")
 
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
 def start():
     _updater = Updater(settings.bot_settings['TOKEN'], use_context=True)
     _updater.dispatcher.add_handler(CommandHandler('hello', hello))
     _updater.dispatcher.add_handler(CommandHandler('start', start))
+    _updater.dispatcher.add_handler(MessageHandler(filters.Filters.all, echo))
     _updater.start_polling()
     _updater.idle()
